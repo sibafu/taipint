8bf74d0f26047cb822d80b760e13fd336f2042c7
toshi <56500993+sibafu@users.noreply.github.com>
2022-04-10 16:17:53 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const display_message = [
+{'ニックはその報酬に満足していない。彼は自己中心的で欲が深い。':'Nick is by no means satisfied with the reward. He is selfish and greedy.'},
+{'「全部で200ドル貸してるの、ボブ。いつ返すつもりなの？」 「ごめん、金欠なんだ。」 「またなの？」':'"You owe me $200 altogether, Bob. When are you going to pay me back?" "Im sorry. Im hard up." "There you go again!"' },
+{'ブライアンが驚いたことに、借金はかなりの額になっていた。':
+'To Brians surprise, his debt amounted to a considerable sum.'},
+{'過去の栄光にしがみつくのはやめないと、取り残されるわよ。':
+'Stop clinging to your glory days, or youll be left behind.'},
+
+{'今日の厳しい現実からは逃れられない以上、それに順応しなければならない。':
+'You cannot escape from todays harsh realities, so you must adapt to them.'},
+
+{'繁栄は永遠に続くものではない。つまり、いつかは終わりを迎えるものだ。':
+'Prosperity does not last forever. In other words, it will come to an end one of these days.'},
+
+{'その職種には相応の資格や能力が必要で、コンピューターが使えることは絶対条件である。':
+'Proper qualifications are required for the position; computer literacy is a must.'},
+
+{'この封筒に履歴書も入れて人事部に提出して下さい。':
+'Enclose your resume in this envelope and submit it to the personnel department.'},
+
+{'物事がうまく行くと信じるなんて君も楽観的だね。可能性はせいぜい50%だよ。':
+'Its optimistic of you to believe that things will work out. At best theres a fifty-fifty chance.'},
+
+{'登録用紙は無料で入手できます。':
+'Registration forms can be obtained free of charge.'},
+{'この講習会に参加をご希望の方は、今すぐお電話下さい。':
+'Please give us a call now if you want to participate in the workshop!'},
+
+{'さらにご質問がありましたら、どうぞご遠慮なくフリーダイヤル1-800-555-1212までお電話ください。':
+'For further inquiries, please feel free to contact us toll-free at 1-800-555-1212.'}
+
+]
